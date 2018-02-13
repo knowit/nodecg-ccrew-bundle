b14d3ebaaebef30092bcc382a76f255c9286f5e8
S. Christoffer Eliesen <christoffer@eliesen.no>
2018-02-13 21:07:03 +0100

    dashboard/breakscreen-2: Finish implementation.

--- extension.js
+++ extension.js
@@ -2,58 +2,12 @@
 
 module.exports = function (nodecg) {
 
-  // let timer = null;
-
-  // /**
-  //  * Starts the countdown at the specified startTime.
-  //  * @param {string} startTime - A formatted time string, such as 1:00 for one hour.
-  //  * @returns {undefined}
-  //  */
-  // function start(startTime) {
-  //   if (running.value) {
-  //     return;
-  //   }
-
-  //   const timeObj = new TimeObject(TimeObject.parseSeconds(startTime));
-  //   if (timeObj.raw <= 0) {
-  //     return;
-  //   }
-
-  //   running.value = true;
-  //   time.value = timeObj;
-  //   timer = setInterval(tick, 1000);
-  // }
-
-  // /**
-  //  * Stops the countdown.
-  //  * @returns {undefined}
-  //  */
-  // function stop() {
-  //   if (!running.value) {
-  //     return;
-  //   }
-
-  //   running.value = false;
-  //   clearInterval(timer);
-  // }
-
-  // /**
-  //  * Ticks the countdown timer down by one second, stopping the timer if it hits zero.
-  //  * @returns {undefined}
-  //  */
-  // function tick() {
-  //   TimeObject.decrement(time.value);
-
-  //   if (time.value.raw <= 0) {
-  //     stop();
-  //   }
-  // }
-
-  const entry_a = nodecg.Replicant('entry_a', {defaultValue: 'Entry one!', persistent: false});
-
-  const first = nodecg.Replicant('first', {defaultValue: 'First entry!', persistent: true});
-  const second = nodecg.Replicant('second', {defaultValue: 'Second entry!', persistent: true});
-  const third = nodecg.Replicant('third', {defaultValue: 'Third entry!', persistent: true});
+  const first = nodecg.Replicant('first', {defaultValue: '', persistent: true});
+  const first_pres = nodecg.Replicant('first', {defaultValue: '', persistent: true});
+  const second = nodecg.Replicant('second', {defaultValue: '', persistent: true});
+  const second_pres = nodecg.Replicant('second', {defaultValue: '', persistent: true});
+  const third = nodecg.Replicant('third', {defaultValue: '', persistent: true});
+  const third_pres = nodecg.Replicant('third', {defaultValue: '', persistent: true});
 
   // const running = nodecg.Replicant('countdownRunning', {defaultValue: false, persistent: false});
 
