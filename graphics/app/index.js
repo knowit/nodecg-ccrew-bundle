09032d7da71853cd6f7ee221e19baedcff44700f
S. Christoffer Eliesen <christoffer@eliesen.no>
2018-02-16 00:35:03 +0100

    breakscreen: Add fourth speaker.
    Yeah this should be done with a list obviously, don't have time right
    now ...

--- graphics/app/index.js
+++ graphics/app/index.js
@@ -8,10 +8,15 @@
 
   const first = nodecg.Replicant('first');
   const first_pres = nodecg.Replicant('first_pres');
+
   const second = nodecg.Replicant('second');
   const second_pres = nodecg.Replicant('second_pres');
+
   const third = nodecg.Replicant('third');
   const third_pres = nodecg.Replicant('third_pres');
+
+  const fourth = nodecg.Replicant('fourth');
+  const fourth_pres = nodecg.Replicant('fourth_pres');
 
   console.log("index.js GO!", event_title, first);
 
@@ -23,9 +28,14 @@
 
     first.on('change', newVal => setHtml('first', newVal));
     first_pres.on('change', newVal => setHtml('first_pres', newVal));
+
     second.on('change', newVal => setHtml('second', newVal));
     second_pres.on('change', newVal => setHtml('second_pres', newVal));
+
     third.on('change', newVal => setHtml('third', newVal));
     third_pres.on('change', newVal => setHtml('third_pres', newVal));
+
+    fourth.on('change', newVal => setHtml('fourth', newVal));
+    fourth_pres.on('change', newVal => setHtml('fourth_pres', newVal));
   }, 1000);
 })();
