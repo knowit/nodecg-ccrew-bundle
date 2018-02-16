41477c376fce592ecfe5c52667ad3a94d9f0bb4d
S. Christoffer Eliesen <christoffer@eliesen.no>
2018-02-16 21:26:21 +0100

    extension: Set default value for fourth speaker.
    This prevents ugly 'undefined' text.

--- extension.js
+++ extension.js
@@ -10,6 +10,8 @@ module.exports = function (nodecg) {
   const second_pres = nodecg.Replicant('second_pres', {defaultValue: '', persistent: true});
   const third = nodecg.Replicant('third', {defaultValue: '', persistent: true});
   const third_pres = nodecg.Replicant('third_pres', {defaultValue: '', persistent: true});
+  const fourth = nodecg.Replicant('third', {defaultValue: '', persistent: true});
+  const fourth_pres = nodecg.Replicant('fourth_pres', {defaultValue: '', persistent: true});
 
   const speaker_name = nodecg.Replicant('speaker_name', {defaultValue: '', persistent: true});
   const presentation_title = nodecg.Replicant('presentation_title', {defaultValue: '', persistent: true});
