16c5ed41faccd9dfe9b49d253d0380a298743e96
S. Christoffer Eliesen <christoffer@eliesen.no>
2018-02-25 20:51:11 +0100

    panelSpeakerList: Only two years range.

--- dashboard/panelSpeakerList.js
+++ dashboard/panelSpeakerList.js
@@ -180,7 +180,7 @@ class Talk extends React.Component {
     setTimeout(() => Materialize.updateTextFields(), 1000);
     $('.datepicker').pickadate({
       selectMonths: true, // Creates a dropdown to control month
-      selectYears: 15, // Creates a dropdown of 15 years to control year,
+      selectYears: 2, // Creates a dropdown of x years to control year,
       today: 'Today',
       clear: 'Clear',
       close: 'Ok',
